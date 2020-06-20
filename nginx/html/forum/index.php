4d2ffbbf07f81d009e831cf9d78105f33bb05878
imgomi <webmaster@mail.gomi.land>
2020-06-20 17:04:39 +0900

    포럼 마크업 작성

---
+++ nginx/html/forum/index.php
@@ -0,0 +1,150 @@
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1280, initial-scale=1.0">
+    <title>계명대학교 :: 게임모바일공학과 홈페이지</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <link href='https://fonts.googleapis.com/css?family=Source Code Pro' rel='stylesheet'>
+</head>
+<body>
+    <div class="wrapper">
+        <header class="forum-page-header-menu-bar">
+            <div class="forum-header-menu-wrap">
+                <h1 class="forum-header-logo-wrap">
+                    <a href="https://www.kmu.ac.kr/" class="kmu-logo-link">계명대학교</a>
+                    <a href="/" class="gmc-logo-link">게임모바일공학과</a>
+                </h1>
+                <nav>
+                    <a href="">소개</a>
+                    <span style="color: #fff;">|</span>
+                    <a href="">연혁</a>
+                    <span style="color: #fff;">|</span>
+                    <a href="">모집요강</a>
+                    <span style="color: #fff;">|</span>
+                    <a href="">포럼</a>
+                </nav>
+                <a href="" class="header-menu-border-btn">로그인</a>
+            </div>
+        </header>
+        <main>
+            <div class="center">
+                <div class="forum-content">
+                    <section class="forum-category-view">
+                        <h4 style="font-size: 24px;margin: 0;">계명대학교 게임모바일공학과 포럼에 오신걸 환영합니다.</h4>
+                        <dl>학과 공지 및 수업 질문과 학생들간의 지식을 나눠 함께 성장하는 공간입니다.</dl>
+                        <div class="forum-category-list">
+
+                            <article>
+                                <div class="forum-category-title"><p>학과 소식</p></div>
+
+                                <div class="forum-sub-category-wrap">
+                                    <div class="forum-category">
+                                        <div class="forum-category-info">
+                                            <div class="forum-sub-category-title">
+                                                <dt>학과 공지사항</dt>
+                                                <dd>학과내 공지사항을 전달하는 게시판입니다.</dd>
+                                            </div>
+                                            <div class="forum-sub-category-count">
+                                                <dt>9,999</dt>
+                                                <p>Posts</p>
+                                            </div>
+                                            <div class="forum-sub-category-recent">
+                                                <div class="forum-sub-category-recent-wrap">
+                                                    <div class="forum-user-profile">
+                                                        <img src="/img/profile/default.jpg" alt="profile image">
+                                                    </div>
+                                                    <div class="forum-user-info">
+                                                        <dt class="forum-article-title">안녕하세요~~!!@@@@@@@@@</dt>
+                                                        <dt class="forum-user-name">운영자</dt>
+                                                        <dt class="forum-article-date">작성일 2020-06-18</dt>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="forum-sub-category-wrap">
+                                    <div class="forum-category">
+                                        <div class="forum-category-info">
+                                            <div class="forum-sub-category-title">
+                                                <dt>공모전 정보</dt>
+                                                <dd>교내외에서 주체하는 공모전 정보를 전달하는 게시판입니다.</dd>
+                                            </div>
+                                            <div class="forum-sub-category-count">
+                                                <dt>9,999</dt>
+                                                <p>Posts</p>
+                                            </div>
+                                            <div class="forum-sub-category-recent">
+                                                <div class="forum-sub-category-recent-wrap">
+                                                    <div class="forum-user-profile">
+                                                        <img src="/img/profile/default.jpg" alt="profile image">
+                                                    </div>
+                                                    <div class="forum-user-info">
+                                                        <dt class="forum-article-title">안녕하세요~~!!@@@@@@@@@</dt>
+                                                        <dt class="forum-user-name">운영자</dt>
+                                                        <dt class="forum-article-date">작성일 2020-06-18</dt>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+
+                        </div>
+
+                        <div class="forum-category-list">
+                            <article>
+                                <div class="forum-category-title"><p>수업 질문&답변</p></div>
+                                <div class="forum-sub-category-wrap">
+                                    <div class="forum-category">
+                                        <div class="forum-category-info">
+                                            <div class="forum-sub-category-title">
+                                                <dt>학과 공지사항</dt>
+                                                <dd>학과내 공지사항을 전달하는 게시판입니다.</dd>
+                                            </div>
+                                            <div class="forum-sub-category-count">
+                                                <dt>9,999</dt>
+                                                <p>Posts</p>
+                                            </div>
+                                            <div class="forum-sub-category-recent">
+                                                <div class="forum-sub-category-recent-wrap">
+                                                    <div class="forum-user-profile">
+                                                        <img src="/img/profile/default.jpg" alt="profile image">
+                                                    </div>
+                                                    <div class="forum-user-info">
+                                                        <dt class="forum-article-title">안녕하세요~~!!@@@@@@@@@</dt>
+                                                        <dt class="forum-user-name">운영자</dt>
+                                                        <dt class="forum-article-date">작성일 2020-06-18</dt>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </section>
+                    <aside class="forum-alert-list">
+                    </aside>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div class="center foot-wrap">
+                <nav>
+                    <a href="">소개</a>
+                    <a href="">연혁</a>
+                    <a href="">모집요강</a>
+                    <a href="">포럼</a>
+
+                    <a class="a-align-right" href="">페이스북</a>
+                    <a class="a-align-right" href="">트위터</a>
+                    <a class="a-align-right" href="">인스타그램</a>
+                </nav>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
