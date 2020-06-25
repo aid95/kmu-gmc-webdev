9465344c35803f1fab14360a9e53af4181140abc
imgomi <webmaster@mail.gomi.land>
2020-06-25 17:12:18 +0900

    포럼 페이지 DB 연결

--- nginx/html/forum/index.php
+++ nginx/html/forum/index.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+    include_once('../utils/mysql.php');
+?>
+
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
@@ -8,143 +13,69 @@
 </head>
 <body>
     <div class="wrapper">
-        <header class="forum-page-header-menu-bar">
-            <div class="forum-header-menu-wrap">
-                <h1 class="forum-header-logo-wrap">
-                    <a href="https://www.kmu.ac.kr/" class="kmu-logo-link">계명대학교</a>
-                    <a href="/" class="gmc-logo-link">게임모바일공학과</a>
-                </h1>
-                <nav>
-                    <a href="">소개</a>
-                    <span style="color: #fff;">|</span>
-                    <a href="">연혁</a>
-                    <span style="color: #fff;">|</span>
-                    <a href="">모집요강</a>
-                    <span style="color: #fff;">|</span>
-                    <a href="">포럼</a>
-                </nav>
-                <a href="" class="header-menu-border-btn">로그인</a>
-            </div>
-        </header>
+        <?php
+        include_once("header.php");
+        ?>
         <main>
             <div class="center">
                 <div class="forum-content">
                     <section class="forum-category-view">
                         <h4 style="font-size: 24px;margin: 0;">계명대학교 게임모바일공학과 포럼에 오신걸 환영합니다.</h4>
                         <dl>학과 공지 및 수업 질문과 학생들간의 지식을 나눠 함께 성장하는 공간입니다.</dl>
-                        <div class="forum-category-list">
+<?php
+    $category_sql = "SELECT * FROM Category";
+    $category_result = mysqli_query($link, $category_sql);
+    while ($category_row = mysqli_fetch_array($category_result)) {
+echo "                        <div class='forum-category-list'>";
+echo "                            <article>";
+echo "                                <div class='forum-category-title'><p>".$category_row[1]."</p></div>";
+        $board_sql = "SELECT * FROM Board WHERE category_pk='".$category_row[0]."'";
+        $board_result = mysqli_query($link, $board_sql);
+        while ($board_row = mysqli_fetch_array($board_result)) {
+echo "                                <div class='forum-sub-category-wrap'>";
+echo "                                    <div class='forum-category'>";
+echo "                                        <div class='forum-category-info'>";
+echo "                                            <div class='forum-sub-category-title'>";
+echo "                                                <dt><a href='/forum/topic/?id=".$board_row[0]."'>".$board_row[2]."</a></dt>";
+echo "                                                <dd>".$board_row[3]."</dd>";
+echo "                                            </div>";
 
-                            <article>
-                                <div class="forum-category-title"><p>학과 소식</p></div>
+/* 게시글 정보 출력 제거
+echo "                                            <div class='forum-sub-category-count'>";
+echo "                                                <dt>9,999</dt>";
+echo "                                                <p>Posts</p>";
+echo "                                            </div>";
+echo "                                            <div class='forum-sub-category-recent'>";
+echo "                                                <div class='forum-sub-category-recent-wrap'>";
+echo "                                                    <div class='forum-user-profile'>";
+echo "                                                        <img src='/img/profile/default.jpg' alt='profile image'>";
+echo "                                                    </div>";
+echo "                                                    <div class='forum-user-info'>";
+echo "                                                        <dt class='forum-article-title'>안녕하세요~~!!@@@@@@@@@</dt>";
+echo "                                                        <dt class='forum-user-name'>운영자</dt>";
+echo "                                                        <dt class='forum-article-date'>작성일 2020-06-18</dt>";
+echo "                                                    </div>";
+echo "                                                </div>";
+echo "                                            </div>";
+*/
 
-                                <div class="forum-sub-category-wrap">
-                                    <div class="forum-category">
-                                        <div class="forum-category-info">
-                                            <div class="forum-sub-category-title">
-                                                <dt>학과 공지사항</dt>
-                                                <dd>학과내 공지사항을 전달하는 게시판입니다.</dd>
-                                            </div>
-                                            <div class="forum-sub-category-count">
-                                                <dt>9,999</dt>
-                                                <p>Posts</p>
-                                            </div>
-                                            <div class="forum-sub-category-recent">
-                                                <div class="forum-sub-category-recent-wrap">
-                                                    <div class="forum-user-profile">
-                                                        <img src="/img/profile/default.jpg" alt="profile image">
-                                                    </div>
-                                                    <div class="forum-user-info">
-                                                        <dt class="forum-article-title">안녕하세요~~!!@@@@@@@@@</dt>
-                                                        <dt class="forum-user-name">운영자</dt>
-                                                        <dt class="forum-article-date">작성일 2020-06-18</dt>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="forum-sub-category-wrap">
-                                    <div class="forum-category">
-                                        <div class="forum-category-info">
-                                            <div class="forum-sub-category-title">
-                                                <dt>공모전 정보</dt>
-                                                <dd>교내외에서 주체하는 공모전 정보를 전달하는 게시판입니다.</dd>
-                                            </div>
-                                            <div class="forum-sub-category-count">
-                                                <dt>9,999</dt>
-                                                <p>Posts</p>
-                                            </div>
-                                            <div class="forum-sub-category-recent">
-                                                <div class="forum-sub-category-recent-wrap">
-                                                    <div class="forum-user-profile">
-                                                        <img src="/img/profile/default.jpg" alt="profile image">
-                                                    </div>
-                                                    <div class="forum-user-info">
-                                                        <dt class="forum-article-title">안녕하세요~~!!@@@@@@@@@</dt>
-                                                        <dt class="forum-user-name">운영자</dt>
-                                                        <dt class="forum-article-date">작성일 2020-06-18</dt>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </article>
-
-                        </div>
-
-                        <div class="forum-category-list">
-                            <article>
-                                <div class="forum-category-title"><p>수업 질문&답변</p></div>
-                                <div class="forum-sub-category-wrap">
-                                    <div class="forum-category">
-                                        <div class="forum-category-info">
-                                            <div class="forum-sub-category-title">
-                                                <dt>학과 공지사항</dt>
-                                                <dd>학과내 공지사항을 전달하는 게시판입니다.</dd>
-                                            </div>
-                                            <div class="forum-sub-category-count">
-                                                <dt>9,999</dt>
-                                                <p>Posts</p>
-                                            </div>
-                                            <div class="forum-sub-category-recent">
-                                                <div class="forum-sub-category-recent-wrap">
-                                                    <div class="forum-user-profile">
-                                                        <img src="/img/profile/default.jpg" alt="profile image">
-                                                    </div>
-                                                    <div class="forum-user-info">
-                                                        <dt class="forum-article-title">안녕하세요~~!!@@@@@@@@@</dt>
-                                                        <dt class="forum-user-name">운영자</dt>
-                                                        <dt class="forum-article-date">작성일 2020-06-18</dt>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </article>
-                        </div>
+echo "                                        </div>";
+echo "                                    </div>";
+echo "                                </div>";
+        }
+echo "                            </article>";
+echo "                        </div>";
+    }
+?>
                     </section>
                     <aside class="forum-alert-list">
                     </aside>
                 </div>
             </div>
         </main>
-        <footer>
-            <div class="center foot-wrap">
-                <nav>
-                    <a href="">소개</a>
-                    <a href="">연혁</a>
-                    <a href="">모집요강</a>
-                    <a href="">포럼</a>
-
-                    <a class="a-align-right" href="">페이스북</a>
-                    <a class="a-align-right" href="">트위터</a>
-                    <a class="a-align-right" href="">인스타그램</a>
-                </nav>
-            </div>
-        </footer>
+        <?php
+        include_once("footer.html");
+        ?>
     </div>
 </body>
 </html>
