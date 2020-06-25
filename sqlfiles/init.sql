dc57358ebcc38054287a6117b0bf768294647f18
imgomi <webmaster@mail.gomi.land>
2020-06-25 14:59:00 +0900

    쿼리 구조 수정

--- sqlfiles/init.sql
+++ sqlfiles/init.sql
@@ -13,22 +13,31 @@ CREATE TABLE Account (
 CREATE TABLE Category (
     pk INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    description VARCHAR(120) NOT NULL,
 
     PRIMARY KEY (pk)
+);
+
+CREATE TABLE Board (
+    pk INT NOT NULL AUTO_INCREMENT,
+    category_pk INT,
+    title VARCHAR(30) NOT NULL,
+    description VARCHAR(120) NOT NULL,
+
+    PRIMARY KEY (pk),
+    FOREIGN KEY (category_pk) REFERENCES Category(pk) ON DELETE SET NULL
 );
 
 CREATE TABLE Article ( 
     pk INT NOT NULL AUTO_INCREMENT,
     user_pk INT,
-    category_pk INT,
+    board_pk INT,
     title VARCHAR(60) NOT NULL,
     content TEXT NOT NULL,
     created_date DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (pk),
     FOREIGN KEY (user_pk) REFERENCES Account(pk) ON DELETE CASCADE,
-    FOREIGN KEY (category_pk) REFERENCES Category(pk) ON DELETE CASCADE
+    FOREIGN KEY (board_pk) REFERENCES Board(pk) ON DELETE CASCADE
 );
 
 CREATE TABLE Comment (
@@ -41,4 +50,4 @@ CREATE TABLE Comment (
     PRIMARY KEY (pk),
     FOREIGN KEY (user_pk) REFERENCES Account(pk) ON DELETE CASCADE,
     FOREIGN KEY (article_pk) REFERENCES Article(pk) ON DELETE CASCADE
-)
+);
